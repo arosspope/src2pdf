34da9c5048adb0a8d5026b8946aa2a103cbe7cac
arosspope <andrew.pope456@gmail.com>
2017-10-26 19:30:57 +1100

    New functionality for the script and docs

--- README.md
+++ README.md
@@ -1,26 +1,65 @@
-# Introduction
+# src2pdf :page_with_curl:
 
-This script allows a user to print source code files within a folder to a pdf file. After specifying a title, author and coding language the generated pdf contains a coverpage, table of contents and user source files.
-I find this script particuarly useful when university assignments require a hardcopy of student code, as it presents your development effort in a neat and ordered format.
+Yet another source code to pdf script using LaTeX.
 
-## Usage
+## Introduction
+### Rationale
 
-After installing the required dependencies as specified below, the script should work out-of-the-box. Personally, I like to put the script somewhere where it can be easily found on the PATH (such as /usr/local/bin).
-Naviagte to the directory containing all your source files and run the following line (note in the following example, source files are written in c).
+Ever needed to hand in a hardcopy of the code you produced in a university assignment? Have you almost destroyed your keyboard trying to format a word document (or similar) containing this same source code?
 
->
-    $ Usage: srcpdf [title] [author] [language]
-    $ /usr/local/bin/srcpdf.sh "Assignment 1" "A.R.P" "C"
->
+Well look no further, `src2pdf` is here to put your troubled mind at ease! Using LaTeX and Bash, present your development efforts in a neat and ordered format using one command!
 
-This will create a sub-directory called 'srcpdf' which contains the pdf and the tex files used to generate the pdf. Please note that **special characters** within the title/author name such as (#, %, $, _, ^, &, }, {) **must be escaped**, or the script will not print correctly.
+### Example
 
-Currently, the supported languages are 'C', 'C++' and 'Python' - but this can be easily extended upon.
+![1](https://i.imgur.com/T4QiEX5m.png) ![2](https://i.imgur.com/YDpsWm9m.png)
+> _Show how fancy you are with a title page, table of contents, and syntax highlighting based on source language_
+
+## Installation
+### Required packages
+
+As this script extensively uses LaTeX, you will need to ensure that it is installed on your system. As an example for Ubuntu, you might install the packages...
+```bash
+$ apt-get install texlive-latex-base texlive-latex-extra
+```
+More generally, this script will require the following packages to be installed: [pdflatex](http://linux.die.net/man/1/pdflatex),  [color](http://www.ctan.org/tex-archive/macros/latex/contrib/xcolor/), [listings](https://en.wikibooks.org/wiki/LaTeX/Source_Code_Listings).
+
+### Finding the script
+
+After installing the required packages, the script should work out-of-the-box after making it executable. Personally, I like to put the script somewhere where it can be easily found on `$PATH` (such as `/usr/local/bin`).
+
+```bash
+$ chomd +x /usr/local/bin/src2pdf.sh
+```
+
+## Operation
+
+Naviagte to your project directory and run the following (assuming the script is on the `$PATH`):
+```bash
+arosspope@(project) $ src2pdf
+```
+You will then be prompted with a series of questions to generate and customise the pdf. Here is an example of the script running:
 
 
-## Dependencies
+```bash
+$ Title (blank to use prm_sim) : PRM Simulator
+$ Author (blank for nothing) : arosspope
+$ Subtitle (blank for nothing) : A Probabilistic RoadMap (PRM) simulator in ROS
+$ Language of files to parse (blank for 'C++') :
+$ Provide a space separated list of extensions to include (default is 'h cpp') :
+$ Re-order files to place header files in front of source files? (y/n) : y
+  Re-ordering files.
+$ Review files found? (y/n) : n
+  Creating tex file.
+  Creating pdf.
+...
+  Renaming output files.
+  Cleaning up.
+Done, output file is 'PRM Simulator.pdf' in this directory
+```
+Please note that _special characters_ `#%$_^&}{)(` within the title, subtitle and/or author positions **must be escaped**, or the script will fail to run.
 
-The following items must be installed for the script to successfully run.
-+ [pdflatex](http://linux.die.net/man/1/pdflatex)
-+ [color](http://www.ctan.org/tex-archive/macros/latex/contrib/xcolor/)
-+ [listings](https://en.wikibooks.org/wiki/LaTeX/Source_Code_Listings)
+Currently, the supported languages are specified by the LaTeX [source code listings](http://en.wikibooks.org/wiki/LaTeX/Source_Code_Listings) page.
+
+## Acknowledgements
+
+The bones of this script comes from the [tutorial](https://samhobbs.co.uk/2017/01/bash-script-generate-pdf-source-code-syntax-highlighting-using-latex) _'BASH Script to generate PDF of Source Code with Syntax Highlighting using LaTeX'_ by Sam Hobbs, 2017.
---
+++ src2pdf.sh
@@ -0,0 +1,232 @@
+#!/usr/bin/env bash
+
+read -p "Title (blank to use ${PWD##*/}) : " answer
+
+if [[ $answer == "" ]]; then
+    title=${PWD##*/}
+else
+    title=$answer
+fi
+
+read -p "Author (blank for nothing) : " answer
+
+if [[ $answer == "" ]]; then
+    author=""
+else
+    author=$answer
+fi
+
+read -p "Subtitle (blank for nothing) : " answer
+
+if [[ $answer == "" ]]; then
+    subtitle=""
+else
+    subtitle=$answer
+fi
+
+# see http://en.wikibooks.org/wiki/LaTeX/Source_Code_Listings, part `Supported languages`
+read -p "Language of files to parse (blank for 'C++') : " answer
+
+if [[ $answer == "" ]]; then
+    lang="C++"
+else
+    lang=$answer
+fi
+
+# if output files already exist, delete them
+if [ -f ./tmp.aux ] || [ -f ./tmp.log ] || [ -f ./tmp.out ] || [ -f ./tmp.pdf ] || [ -f ./tmp.toc ] ; then
+    echo "  Removing old output files."
+    rm ./tmp.*
+fi
+
+tex_file=$(mktemp) ## Random temp file name
+
+if [ $? -ne 0 ]; then
+    echo "  ERROR: failed to create temporary file."
+    exit 1;
+fi
+
+## Begin Tex setup
+cat << EOF > $tex_file
+
+\documentclass[titlepage]{article}
+\usepackage[utf8]{inputenc}
+
+\usepackage{titling}
+\newcommand{\subtitle}[1]{%
+  \posttitle{%
+    \par\end{center}
+    \begin{center}\large#1\end{center}
+    \vskip0.5em}%
+}
+
+\author{$author}
+\title{$title}
+\subtitle{$subtitle}
+
+\usepackage{listings}
+\usepackage[usenames,dvipsnames]{color} %% Allow color names
+\lstdefinestyle{customasm}{
+  belowcaptionskip=1\baselineskip,
+  xleftmargin=\parindent,
+  language=$lang,
+  breaklines=true,                      %% Wrap long lines
+  basicstyle=\footnotesize\ttfamily,
+  commentstyle=\itshape\color{Gray},
+  stringstyle=\color{Black},
+  keywordstyle=\bfseries\color{OliveGreen},
+  identifierstyle=\color{blue},
+  xleftmargin=-8em,
+}
+\usepackage[colorlinks=true,linkcolor=blue]{hyperref}
+\usepackage{fancyhdr}
+\pagestyle{fancy}
+\lhead{$title}
+\rhead{$author}
+\begin{document}
+
+\maketitle
+
+\pagenumbering{roman}
+\tableofcontents
+
+\newpage
+\setcounter{page}{1}
+\pagenumbering{arabic}
+
+EOF
+## End Tex Setup
+
+# ask the user which file extensions to include
+
+read -p "Provide a space separated list of extensions to include (default is 'h cpp') : " answer
+
+if [[ $answer == "" ]]; then
+    answer="h cpp"
+fi
+
+# replace spaces with double escaped pipe using substring replacement  http://www.tldp.org/LDP/abs/html/parameter-substitution.html
+
+extensions="${answer// /\\|}"
+
+###############
+
+# FINDING FILES TO INCLUDE
+# inline comments http://stackoverflow.com/questions/2524367/inline-comments-for-bash#2524617
+# not all of the conditions below are necessary now that the regex for c++ files has been added, but they don't harm
+
+filesarray=(
+$(
+find .                                          `# find files in the current directory` \
+        -type f                                 `# must be regular files` \
+        -regex ".*\.\($extensions\)"            `# only files with the chosen extensions (.h, .cpp and .qml) by default` \
+        ! -regex ".*/\..*"                      `# exclude hidden directories - anything slash dot anything (Emacs regex on whole path https://www.emacswiki.org/emacs/RegularExpression)` \
+        ! -name ".*"                            `# not hidden files` \
+        ! -name "*~"                            `# don't include backup files` \
+        ! -name 'src2pdf'                       `# not this file if it's in the current directory`
+))
+
+###############
+
+# sort the array https://stackoverflow.com/questions/7442417/how-to-sort-an-array-in-bash#11789688
+# internal field separator $IFS https://bash.cyberciti.biz/guide/$IFS
+
+IFS=$'\n' filesarray=($(sort <<<"${filesarray[*]}"))
+unset IFS
+
+###############
+
+## TODO: This doesn't make much sense for other languages
+read -p "Re-order files to place header files in front of source files? (y/n) : " answer
+
+if [[ ! $answer == "n" ]] && [[ ! $answer == "N" ]] ; then
+    echo "  Re-ordering files."
+
+    # if this element is a .cpp file, check the next element to see if it is a matching .h file
+    # if it is, swap the order of the two elements
+    re="^(.*)\.c(pp)?$"
+
+    # this element is ${filesarray[$i]}, next element is ${filesarray[$i+1]}
+    for (( i=0; i<=$(( ${#filesarray[@]} -1 )); i++ ))
+    do
+        # if the element is a .cpp file, check the next element to see if it is a matching .h file
+        if [[ ${filesarray[$i]} =~ $re ]]; then
+            header=${BASH_REMATCH[1]}
+            header+=".h"
+            if [[ ${filesarray[$i+1]} == $header ]]; then
+                # replace the next element in the array with the current element
+                filesarray[$i+1]=${filesarray[$i]}
+                # replace the current element in the array with $header
+                filesarray[$i]=$header
+            fi
+        fi
+    done
+fi
+
+###############
+
+# Change ./foo/bar.src to foo/bar.src
+IFS=$'\n' filesarray=($(sed 's/^\..//' <<<"${filesarray[*]}"))
+unset IFS
+
+###############
+
+read -p "Review files found? (y/n) : " answer
+
+if [[ $answer == "y" ]] || [[ $answer == "Y" ]] ; then
+
+    echo "  The following files will be included in the document."
+
+    for i in "${filesarray[@]}"
+    do
+        echo $i
+    done
+
+    # allow the user to abort
+    read -p "Proceed? (y/n) : " answer
+    if [[ $answer == "n" ]] || [[ $answer == "N" ]] ; then
+        exit 0
+    fi
+
+fi
+
+###############
+
+# create a .tex file with each section on its own page
+
+echo "  Creating tex file."
+
+for i in "${filesarray[@]}"
+do
+    name=$(echo $i | sed 's/\_/\\_/g') # Escape filename with underscores
+    echo "\newpage" >> $tex_file   # start each section on a new page
+    echo "\section{$name}" >> $tex_file  # create a section for each source file
+    echo "\lstinputlisting[style=customasm]{$i}" >>$tex_file # place the contents of each file in a listing
+done
+
+echo "\end{document}" >> $tex_file
+
+###############
+
+# run pdflatex twice to produce TOC
+echo "  Creating pdf."
+echo
+
+pdflatex $tex_file -output-directory . && pdflatex $tex_file -output-directory .
+
+if [ $? -ne 0 ]; then
+    echo "  ERROR: pdflatex command failed, refer to tmp.log for more information."
+    exit 1;
+fi
+
+###############
+
+echo "  Renaming output files."
+
+mv tmp.pdf "$title.pdf"
+
+echo "  Cleaning up."
+
+rm ./tmp.*
+
+echo "Done, output file is '$title.pdf' in this directory"
